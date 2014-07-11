d821bfede8de58a2c16e3be5242ac16a3565c37b
Focus <jiaojizong@vip.qq.com>
2014-07-11 08:50:34 +0800

    项目第一次提交，但是项目存在问题。

---
+++ springmvc3/src/com/mvc/entity/Student.java
@@ -0,0 +1,46 @@
+package com.mvc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Basic(optional = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Integer id;
+	@Column(name = "name")
+	private String uname;
+	@Column(name = "upsw")
+	private String upsw;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUpsw() {
+		return upsw;
+	}
+	public void setUpsw(String upsw) {
+		this.upsw = upsw;
+	}
+	
+
+}
